3a0312df03f8d8fc25c3402ffec63a79f7e85ade
dipto01 <diptobhowmik77@gmail.com>
2026-03-09 10:28:02 +0600

    login verification & home page setup!

--- scripts/loginScript.js
+++ scripts/loginScript.js
@@ -1,1 +1,16 @@
-console.log("Js connected!")
+// login verifier
+let loginBtn = document.getElementById('login-btn')
+loginBtn.addEventListener("click", () =>{
+    console.log("login Clicked!")
+    // s-1 get username
+    let userId = document.getElementById('user-Id')
+
+    // s-2 get pass
+    let pass = document.getElementById('pass')
+
+    //verify upon click!
+    if(userId.value == "admin" && pass.value == "admin123"){
+        window.location.assign("/home.html")
+    }
+    else alert("Wrong Credentials!")
+})
