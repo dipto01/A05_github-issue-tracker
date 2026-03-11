ec585ddd3873b1f64d5ab354b67b342c4384e44e
dipto01 <diptobhowmik77@gmail.com>
2026-03-11 10:38:43 +0600

    Search functionality added!!!

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -11,7 +11,7 @@ let cardContainer = document.getElementById('card-container')
 
 // loading bar
 let loading = document.getElementById('loading')
-console.log(loading.classList)
+// console.log(loading.classList)
 loading.classList.remove('hidden')
 
 function loadingToggle (status) {
@@ -31,8 +31,7 @@ function loadingToggle (status) {
 let apiUrlAll = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 
 function loadAllIssues ()  {
-    //add loading
-    loadingToggle(true)
+    
 fetch(apiUrlAll)
     .then((response) => response.json())
     .then((dataRaw) => {
@@ -153,7 +152,9 @@ fetch(apiUrlAll)
         }
     })
     //done loading!
-    loadingToggle(false)
+    setTimeout(() => {
+        loadingToggle(false)
+    }, 100)
 }
 
 
@@ -167,6 +168,8 @@ fetch(apiUrlAll)
         cardContainer.innerHTML = " "
         allBtn.classList.add("btn-primary")
         ississueCounter.innerText = "50"
+        //add loading
+    loadingToggle(true)
         loadAllIssues()
     })
 
@@ -188,8 +191,7 @@ document.addEventListener("DOMContentLoaded", allShow)
 
 
 function loadOpenIssues ()  {
-    //add loading
-    loadingToggle(true)
+    
 fetch(apiUrlAll)
     .then((response) => response.json())
     .then((dataRaw) => {
@@ -313,7 +315,9 @@ fetch(apiUrlAll)
         }
     })
     //done loading!
-    loadingToggle(false)
+        setTimeout(() => {
+        loadingToggle(false)
+    }, 100)
 }
 
 //show if Open clicked
@@ -326,14 +330,15 @@ fetch(apiUrlAll)
         cardContainer.innerHTML = " "
         openBtn.classList.add("btn-primary")
         ississueCounter.innerText = "44"
+        //add loading
+    loadingToggle(true)
         loadOpenIssues()
     })
 
 // Close Issues!!!!!!!!!!!!!!!!!!!!!!
 
 function loadClosedIssues ()  {
-    //add loading
-    loadingToggle(true)
+    
 fetch(apiUrlAll)
     .then((response) => response.json())
     .then((dataRaw) => {
@@ -457,7 +462,9 @@ fetch(apiUrlAll)
         }
     })
     //done loading!
-    loadingToggle(false)
+        setTimeout(() => {
+        loadingToggle(false)
+    }, 100)
 }
 
 //show if Closed clicked
@@ -470,6 +477,8 @@ fetch(apiUrlAll)
         cardContainer.innerHTML = " "
         closedBtn.classList.add("btn-primary")
         ississueCounter.innerText = "6"
+        //add loading
+    loadingToggle(true)
         loadClosedIssues()
     })
 
@@ -588,3 +597,151 @@ function loadWordDetail(id){
     let modal = document.getElementById('my_modal_5')
     modal.showModal()
 }
+
+
+
+// ----------Search-------------
+
+let searchInput = document.getElementById('searchInput')
+
+
+searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+    //clear previous!
+    cardContainer.innerHTML = " "
+    //loading
+    loading.classList.remove('hidden')
+
+        let value = searchInput.value.toLowerCase()
+        
+    // fetch from api!
+    let searchUrl = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${value}`
+    fetch(searchUrl)
+        .then((result) => result.json())
+        .then((sResult) => {
+        //loop!
+        console.log(sResult)
+        //counter reset!
+        ississueCounter.innerText = sResult.total
+        // console.log(sResult.total)
+
+        for(let i=0; i<sResult.data.length; i++){
+        //id no
+        let cardId = sResult.data[i].id
+            console.log(cardId)
+        // title
+        let cardTitle = sResult.data[i].title       //text
+
+        // description
+        let cardDes = sResult.data[i].description      //text
+
+        // status
+        let cardStatus = sResult.data[i].status     //open or close text
+        let topColor = ""
+        let statusImg = ""
+        if(cardStatus == "open"){
+            topColor = "shadow-lg rounded-md border-t-5 border-[#00A96E] p-4"
+            statusImg = "./assets/Open-Status.png"
+        }
+        else if (cardStatus == "closed"){
+            topColor = "shadow-lg rounded-md border-t-5 border-[#A855F7] p-4"
+            statusImg = "./assets/Closed- Status .png"
+        }
+        // priority
+        let cardPriority = sResult.data[i].priority     //high or low or medium text
+        let priorityClass = ""
+        if(cardPriority == "high"){
+            priorityClass = "text-[#EF4444] bg-[#FEECEC] px-4 rounded-xl"
+
+        }
+        else if(cardPriority == "medium"){
+            priorityClass = "text-[#F59E0B] bg-[#FFF6D1] px-4 rounded-xl"
+        }
+        else if(cardPriority == "low"){
+            priorityClass = "text-[#9CA3AF] bg-[#EEEFF2] px-4 rounded-xl"
+        }
+
+        // !! labels !!array
+        let labelText1 = ""
+        let labelStyle1 = ""
+        let labelArtClass1 = ""
+        let labelArtStyle1 = ""
+
+        let labelText2 = ""
+        let labelStyle2 = ""
+        let labelArtClass2 = ""
+        let labelArtStyle2 = ""
+
+        let labelText3 = ""
+        let labelStyle3 = ""
+        let labelArtClass3 = ""
+        let labelArtStyle3 = ""
+
+        let cardLabels = () => {
+            for(let label of sResult.data[i].labels){
+                if(label == "bug"){         //for BUG
+                    labelText1 = "BUG"
+                    labelStyle1 = "text-[#EF4444] font-semibold bg-[#FEECEC] px-3 rounded-2xl border-2 border-[#FECACA]"
+                    labelArtClass1 = "fa-solid fa-bug"
+                    labelArtStyle1 = "color: #ef4444;"
+                }
+                else if(label == "help wanted") {       //for help wanted
+                    labelText2 = "HELP WANTED"
+                    labelStyle2 = "text-[#D97706] font-semibold bg-[#FFF8DB] px-3 rounded-2xl border-2 border-[#FDE68A]"
+                    labelArtClass2 = "fa-solid fa-life-ring"
+                    labelArtStyle2 = "color: #d97706;"
+                }
+                else if (label == "enhancement") {      //for enhancement
+                    labelText3 = "ENHANCEMENT"
+                    labelStyle3 = "text-[#00A96E] font-semibold bg-[#DEFCE8] px-3 rounded-2xl border-2 border-[#BBF7D0]"
+                    labelArtClass3 = "fa-solid fa-wand-sparkles"
+                    labelArtStyle3 = "color: #00a96e;"
+                } 
+            }
+        }
+        cardLabels() // returns some label
+
+        //author
+        let cardAuthor = sResult.data[i].author     //text
+
+        //Created at
+        let cardDate = sResult.data[i].createdAt
+
+
+        //Card Creation!
+        let cardDiv = document.createElement('div')
+        //card Editing!
+        cardDiv.innerHTML = `
+            <div class="${topColor}" onclick="loadWordDetail(${cardId})">
+            <div class="flex justify-between">
+                <img src="${statusImg}" alt="">
+                <p class="${priorityClass}">${cardPriority.toUpperCase()}</p>
+            </div>
+            <div class="mt-3">
+                <h3 class="font-semibold mb-2 text-lg">${cardTitle}</h3>
+                <p class="text-[#64748B]">${cardDes}</p>
+            </div>
+            <div class="flex gap-3 mt-3">
+                <p class="${labelStyle1}"><i class="${labelArtClass1}" style="${labelArtStyle1}"></i> ${labelText1}</p>
+                <p class="${labelStyle2}"><i class="${labelArtClass2}" style="${labelArtStyle2}"></i> ${labelText2}</p>
+                <p class="${labelStyle3}"><i class="${labelArtClass3}" style="${labelArtStyle3}"></i> ${labelText3}</p>
+            </div>
+            <hr class="mt-4 border-gray-300 border mb-4">
+            <div>
+                <p class="text-[#64748B]">#${cardId} by ${cardAuthor}</p>
+                <p class="text-[#64748B]">${cardDate}</p>
+            </div>
+        </div>
+        `
+        //Card Append!!!
+        //s-1 select location to add
+        // let cardContainer = document.getElementById('card-container')
+        //s-2 append!
+        cardContainer.appendChild(cardDiv)
+        }
+    })
+}
+    setTimeout(() => {
+        loading.classList.add('hidden')
+    }, 500)
+})
